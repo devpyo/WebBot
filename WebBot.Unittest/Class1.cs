eff774706b7880c2677fffb7686271e0fef55ff2
pyo_red <dev.pyo@gmail.com>
2018-07-27 11:35:54 +0900

    유닛테스트 프로젝트 추가

---
+++ WebBot.Unittest/Class1.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace WebBot.Unittest
+{
+    public class Class1
+    {
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(7)]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+    }
+}
